6539b3b5b3deb9af92dc4ec512af9c3f984beee6
Ahmed Mahmoud <a.samyabdelhay@gmail.com>
2025-12-30 14:12:12 +0100

    Edusync V5 migration: deployment

--- scripts/apply_client_config.js
+++ scripts/apply_client_config.js
@@ -78,7 +78,7 @@ if (fs.existsSync(moodleConfigPath)) {
 
     // Update app_id based on platform
     if (platform === 'ios' && client.appId.ios) {
-        moodleConfig.app_id = client.appId.ios;
+        moodleConfig.app_id = client.appId.android;
         console.log(`Setting app_id to iOS Bundle ID: ${client.appId.android}`);
     } else {
         moodleConfig.app_id = client.appId.android;
